fc3d84e2eac1a81fbe4ca2244733612415b34e99
Brad Gibson <bRad@humanenginuity.com>
2016-09-20 19:46:04 -0700

    Fixed regex test

--- Sources/Modules/Hello/Hello.cpp
+++ Sources/Modules/Hello/Hello.cpp
@@ -5,5 +5,5 @@
 
 std::string Hello::Greet() const
 {
-	return "Hello, " + std::to_string(std::numeric_limits<size_t>::digits) + "-bit World!\n";
+	return "Hello " + std::to_string(std::numeric_limits<size_t>::digits) + "-bit World!\n";
 }
