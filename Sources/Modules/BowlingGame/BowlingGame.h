aa40c82d8a25755b3fd646a52be46c7bb8397974
Brad Gibson <bRad@humanenginuity.com>
2016-12-02 07:18:26 -0800

    Inital BowlingGameKata commit

---
+++ Sources/Modules/BowlingGame/BowlingGame.h
@@ -0,0 +1,12 @@
+#ifndef BOWLING_GAME_H
+#define BOWLING_GAME_H
+
+#include "Helpers/AbbreviatedTypes.h"
+
+/// BowlingGame class contains all information about 1 game (10 frames) of bowling
+
+
+/// \example ../Tests/Unit/BowlingGame.UnitTests.cpp
+/// This is an example of how to use the BowlingGame class.
+
+#endif //BOWLING_GAME_H
---
+++ Sources/Modules/Helpers/AbbreviatedTypes.h
@@ -0,0 +1,21 @@
+#ifndef ABBREVIATED_TYPES_H
+#define ABBREVIATED_TYPES_H
+#include <atomic>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isz = ptrdiff_t;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usz = size_t;
+
+using f32 = float;
+using f64 = double;
+using f80 = long double;
+
+#endif // ABBREVIATED_TYPES_H
