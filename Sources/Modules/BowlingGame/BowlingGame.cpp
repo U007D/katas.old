2f2a4d4b9b7808fae66adab6ed869b8d872b8ede
Brad Gibson <brad@humanenginuity.com>
2016-12-08 07:01:57 -0800

    1-roll game test: PASS

--- Sources/Modules/BowlingGame/BowlingGame.cpp
+++ Sources/Modules/BowlingGame/BowlingGame.cpp
@@ -1,6 +1,6 @@
+#include <numeric>
 #include "BowlingGame.h"
 
 u32 BowlingGame::Score(const std::vector<u8>& rolls) {
-    (void)rolls;
-    return 0;
+    return std::accumulate(std::begin(rolls), std::end(rolls), 0_u32);
 }
--- Sources/Tests/Unit/BowlingGame.UnitTests.cpp
+++ Sources/Tests/Unit/BowlingGame.UnitTests.cpp
@@ -8,13 +8,23 @@ SCENARIO("BowlingGame unit tests")
     {
         auto game = BowlingGame();
 
-        WHEN("a game consisting of all gutter-balls is rolled")
+        WHEN("a game of all gutter-balls is rolled")
         {
             auto score = game.Score({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
 
             THEN("the score should be zero")
             {
                 REQUIRE(score == 0);
+            }
+        }
+
+        AND_WHEN("a game of all ones is rolled")
+        {
+            auto score = game.Score({1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1});
+
+            THEN("the score should be 20")
+            {
+                REQUIRE(score == 20);
             }
         }
     }
