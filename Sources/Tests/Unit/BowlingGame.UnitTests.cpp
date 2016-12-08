ecf344381cfd65ebd8fd029063853430237ace16
Brad Gibson <bRad@humanenginuity.com>
2016-12-08 00:11:39 -0800

    Strike test: PASS

--- Sources/Tests/Unit/BowlingGame.UnitTests.cpp
+++ Sources/Tests/Unit/BowlingGame.UnitTests.cpp
@@ -23,6 +23,7 @@ SCENARIO("BowlingGame unit tests")
             REQUIRE(game.Score() == 20);
         }
     }
+
     GIVEN("a game consisting of a spare in frame 1, followed by a roll of 3, followed by all gutter balls")
     {
         auto game = BowlingGame({0, 10, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
@@ -30,6 +31,16 @@ SCENARIO("BowlingGame unit tests")
         THEN("the score should be 16")
         {
             REQUIRE(game.Score() == 16);
+        }
+    }
+
+    GIVEN("a game consisting of a strike in the first frame, followed by rolls of 3 & 4, followed by all gutter balls")
+    {
+        auto game = BowlingGame({10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+
+        THEN("the score should be 24")
+        {
+            REQUIRE(game.Score() == 24);
         }
     }
 }
