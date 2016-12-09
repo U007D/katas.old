3440482305e3bf79b6642d94b03d0978c3b48b46
Brad Gibson <bRad@humanenginuity.com>
2016-12-08 19:41:45 -0800

    FAILED: imperative, stateful approach

--- Sources/Modules/BowlingGame/BowlingGame.h
+++ Sources/Modules/BowlingGame/BowlingGame.h
@@ -1,10 +1,22 @@
 #ifndef BOWLING_GAME_H
 #define BOWLING_GAME_H
 
+#include <array>
+#include <3rdParty/variant/variant>
 #include "Helpers/AbbreviatedTypes.h"
 
-/// BowlingGame class contains all information about 1 game (10 frames) of bowling
+namespace xstd = std::experimental;
 
+/// BowlingGame class contains all information about 1 game (10 frames) of bowling
+class BowlingGame
+{
+public:
+    using StandardFrame = std::array<u8, 2>;
+    using LastFrame = std::array<u8, 3>;
+    using Frame = xstd::variant<StandardFrame, LastFrame>;
+    static u8 constexpr FRAMES_PER_GAME = 10;
+    u32 Score(std::array<Frame,FRAMES_PER_GAME> frames);
+};
 
 /// \example ../Tests/Unit/BowlingGame.UnitTests.cpp
 /// This is an example of how to use the BowlingGame class.
--- Sources/Tests/Unit/BowlingGame.UnitTests.cpp
+++ Sources/Tests/Unit/BowlingGame.UnitTests.cpp
@@ -6,4 +6,14 @@
 
 SCENARIO("BowlingGame unit tests")
 {
+    GIVEN("a bowling game with no scores")
+    {
+        auto rolls = {{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
+
+            THEN("the score should be 0")
+            {
+                REQUIRE(BowlingGame::Score(rolls) == 0);
+            }
+        }
+    }
 }
