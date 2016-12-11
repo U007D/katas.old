1fa4cfd1520dd60b075d620881000344dd951c4e
Brad Gibson <bRad@humanenginuity.com>
2016-12-10 22:08:32 -0800

    Consolidated Exceptions.h

--- Sources/Modules/Helpers/Exceptions.h
+++ Sources/Modules/Helpers/Exceptions.h
@@ -1,6 +1,18 @@
 #ifndef EXCEPTIONS_H
 #define EXCEPTIONS_H
 
-#include "NotImplementedException.h"
+#include <stdexcept>
 
-#endif // EXCEPTIONS_H
+class NotImplementedException : public std::runtime_error
+{
+public:
+    NotImplementedException() : runtime_error("Method not implemented") {}
+};
+
+class InvalidLengthException : public std::runtime_error
+{
+public:
+    InvalidLengthException() : runtime_error("Object length is invalid (too long or too short)") {}
+};
+
+#endif //EXCEPTIONS_H
