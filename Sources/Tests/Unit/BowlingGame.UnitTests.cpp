b35469a4f6662f5f16afe997d0ac6b2c087d34c6
bRad <brad@humanenginuity.com>
2016-12-21 06:53:35 -0800

    Gutterball test: FAIL

--- Sources/Tests/Unit/BowlingGame.UnitTests.cpp
+++ Sources/Tests/Unit/BowlingGame.UnitTests.cpp
@@ -4,4 +4,16 @@
 
 SCENARIO("BowlingGame unit tests")
 {
+    GIVEN("bowling game scoring system")
+    {
+        WHEN("all gutterballs are rolled")
+        {
+            auto score = Score({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
+
+            THEN("the score should be 0")
+            {
+                REQUIRE(score == 0);
+            }
+        }
+    }
 }
