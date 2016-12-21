2d3d4b354a6c68d160f2bce176e361c9854081dc
bRad <brad@humanenginuity.com>
2016-12-21 06:56:57 -0800

    Gutterball test: PASS

--- Sources/Modules/BowlingGame/BowlingGame.cpp
+++ Sources/Modules/BowlingGame/BowlingGame.cpp
@@ -1,2 +1,7 @@
 #include "BowlingGame.h"
 
+u16 Score(const std::vector<u8>& rolls)
+{
+    (void)rolls;    //To satisfy strict compiler (-Werror, -Wunused-parameter)
+    return 0;
+}
--- Sources/Modules/BowlingGame/BowlingGame.h
+++ Sources/Modules/BowlingGame/BowlingGame.h
@@ -1,10 +1,11 @@
 #ifndef BOWLING_GAME_H
 #define BOWLING_GAME_H
 
+#include <vector>
 #include "Helpers/UserDefinedTypes.h"
 
 /// BowlingGame class contains all information about 1 game (10 frames) of bowling
-
+u16 Score(const std::vector<u8>& rolls);
 
 /// \example ../Tests/Unit/BowlingGame.UnitTests.cpp
 /// This is an example of how to use the BowlingGame class.
