268b7f9dd7f413f7bb7d6578f1126b0f5b97eac9
bRad <brad@humanenginuity.com>
2016-12-25 23:16:36 -0800

    spare test: PASS

--- src/modules/lib.rs
+++ src/modules/lib.rs
@@ -1,10 +1,23 @@
-fn game_score(rolls: &Vec<u8>) -> u16 {
-    rolls.iter().map(|el| { *el as u16 }).sum()
+fn game_score(rolls: &[u8]) -> u16 {
+    frame_score(rolls, 1)
+}
+
+fn frame_score(rolls: &[u8], frame: u8) -> u16 {
+
+    if frame > 10 || rolls.is_empty() {
+        0
+    }
+    else {
+        rolls.iter()
+            .take(if rolls[0] + rolls[1] == 10 { 3 } else { 2 })
+            .fold(0u16, | sum, el | sum + *el as u16)
+        + frame_score(&rolls[2..], frame + 1)
+    }
 }
 
 #[test]
 fn gutterball_game() {
-    let result = game_score(&vec![0u8; 10]);
+    let result = game_score(&[0u8; 10]);
     assert!(result == 0);
 }
 
@@ -12,4 +25,10 @@ fn gutterball_game() {
 fn one_ball_game() {
     let result = game_score(&vec![1u8; 20]);
     assert!(result == 20);
+}
+
+#[test]
+fn spare_followed_by_3_rest_gutterball_game() {
+    let result = game_score(&vec![5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    assert!(result == 16);
 }
