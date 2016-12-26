c58c4cdf86bd718d3d1042eb68494c2e9e7b3064
bRad <brad@humanenginuity.com>
2016-12-25 16:09:31 -0800

    gutterball test: PASS

--- src/modules/lib.rs
+++ src/modules/lib.rs
@@ -1,9 +1,9 @@
-
-fn hello() -> String {
-    format!("Hello, {}-bit world!", std::usize::MAX)
+fn game_score(rolls: &Vec<u8>) -> u16 {
+    0
 }
 
 #[test]
-fn hello_result() {
-    assert!(hello().is_empty() == false);
+fn gutterball_game() {
+    let result = game_score(&vec![0u8; 10]);
+    assert!(result == 0);
 }
