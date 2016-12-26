7856bd631218fd15b936b6bc9d6a1e42bd405088
bRad <brad@humanenginuity.com>
2016-12-25 16:53:59 -0800

    one-ball test: PASS

--- src/modules/lib.rs
+++ src/modules/lib.rs
@@ -1,9 +1,15 @@
 fn game_score(rolls: &Vec<u8>) -> u16 {
-    0
+    rolls.iter().map(|el| { *el as u16 }).sum()
 }
 
 #[test]
 fn gutterball_game() {
     let result = game_score(&vec![0u8; 10]);
     assert!(result == 0);
+}
+
+#[test]
+fn one_ball_game() {
+    let result = game_score(&vec![1u8; 20]);
+    assert!(result == 20);
 }
