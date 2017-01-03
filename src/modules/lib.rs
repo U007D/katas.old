417b016214118c5da1f6cb07a565a9bc041a4d66
bRad <brad@humanenginuity.com>
2017-01-03 08:37:21 -0500

    One-ball test: PASS

--- src/modules/lib.rs
+++ src/modules/lib.rs
@@ -1,9 +1,16 @@
 
 pub fn score(rolls: &[u8]) -> u16 {
-    return 0;
+    return rolls.iter()
+                .map(|el| *el as u16)
+                .sum();
 }
 
 #[test]
 fn all_gutterballs_yield_zero() {
     assert!(score(&[20; 0]) == 0);
+}
+
+#[test]
+fn all_one_balls_yields_20() {
+    assert!(score(&[20; 1]) == 20);
 }
