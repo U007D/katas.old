916505d85152dfcc78e85a05d962722ec01a9606
bRad <brad@humanenginuity.com>
2017-01-04 07:11:24 -0500

    Strike test: PASS

--- src/modules/lib.rs
+++ src/modules/lib.rs
@@ -9,10 +9,10 @@ fn score_frames(rolls: &[u8], frame: u8) -> u16 {
         rolls.iter()
                 .take(if rolls.iter()
                         .take(2)
-                        .sum::<u8>() == 10 { 3 } else { 2 })
+                        .sum::<u8>() >= 10 { 3 } else { 2 })
                 .sum::<u8>() as u16
             +
-                score_frames(&rolls[std::cmp::min(2, rolls.len())..], frame + 1)
+                score_frames(&rolls[std::cmp::min(if rolls[0] == 10 { 1 } else { 2 }, rolls.len())..], frame + 1)
     }
 }
 
@@ -28,5 +28,10 @@ fn all_one_balls_yields_20() {
 
 #[test]
 fn spare_then_3_followed_by_gutterballs_yields_16() {
-    assert!(score_game(&[5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 16);
+    assert!(score_game(&[5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 16);
+}
+
+#[test]
+fn strike_then_3_then_4_followed_by_gutterballs_yields_24() {
+    assert!(score_game(&[10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 24);
 }
