8e5ebddf8ca410d63ab0710430602a0fa48ba909
bRad <brad@humanenginuity.com>
2017-01-04 06:52:34 -0500

    Spare test: PASS

--- src/modules/lib.rs
+++ src/modules/lib.rs
@@ -1,16 +1,32 @@
+pub fn score_game(rolls: &[u8]) -> u16 {
+    score_frames(rolls, 1)
+}
 
-pub fn score(rolls: &[u8]) -> u16 {
-    return rolls.iter()
-                .map(|el| *el as u16)
-                .sum();
+fn score_frames(rolls: &[u8], frame: u8) -> u16 {
+    if frame > 10 {
+        0
+    } else {
+        rolls.iter()
+                .take(if rolls.iter()
+                        .take(2)
+                        .sum::<u8>() == 10 { 3 } else { 2 })
+                .sum::<u8>() as u16
+            +
+                score_frames(&rolls[std::cmp::min(2, rolls.len())..], frame + 1)
+    }
 }
 
 #[test]
 fn all_gutterballs_yield_zero() {
-    assert!(score(&[20; 0]) == 0);
+    assert!(score_game(&[0; 20]) == 0);
 }
 
 #[test]
 fn all_one_balls_yields_20() {
-    assert!(score(&[20; 1]) == 20);
+    assert!(score_game(&[1; 20]) == 20);
+}
+
+#[test]
+fn spare_then_3_followed_by_gutterballs_yields_16() {
+    assert!(score_game(&[5, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == 16);
 }
